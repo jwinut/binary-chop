c8a86c03ac3157ded59413380cefa416a0097e0f
Winut Jiraruekmongkol <nut.winut@gmail.com>
2023-10-22 19:03:44 +0700

    add chop_test from codekata into testChop, make green

--- binary-chop/src/test/java/BinaryChopTest.java
+++ binary-chop/src/test/java/BinaryChopTest.java
@@ -15,4 +15,29 @@ public class BinaryChopTest {
         assertEquals(0, chopper.chop(1, new int[]{1}));
         assertEquals(1, chopper.chop(2, new int[]{1, 2, 3, 4}));
     }
+
+    @Test
+    public void testChop() {
+        assertEquals(-1, chopper.chop(3, new int[]{}));
+        assertEquals(-1, chopper.chop(3, new int[]{1}));
+        assertEquals(0,  chopper.chop(1, new int[]{1}));
+
+        assertEquals(0,  chopper.chop(1, new int[]{1, 3, 5}));
+        assertEquals(1,  chopper.chop(3, new int[]{1, 3, 5}));
+        assertEquals(2,  chopper.chop(5, new int[]{1, 3, 5}));
+        assertEquals(-1, chopper.chop(0, new int[]{1, 3, 5}));
+        assertEquals(-1, chopper.chop(2, new int[]{1, 3, 5}));
+        assertEquals(-1, chopper.chop(4, new int[]{1, 3, 5}));
+        assertEquals(-1, chopper.chop(6, new int[]{1, 3, 5}));
+
+        assertEquals(0,  chopper.chop(1, new int[]{1, 3, 5, 7}));
+        assertEquals(1,  chopper.chop(3, new int[]{1, 3, 5, 7}));
+        assertEquals(2,  chopper.chop(5, new int[]{1, 3, 5, 7}));
+        assertEquals(3,  chopper.chop(7, new int[]{1, 3, 5, 7}));
+        assertEquals(-1, chopper.chop(0, new int[]{1, 3, 5, 7}));
+        assertEquals(-1, chopper.chop(2, new int[]{1, 3, 5, 7}));
+        assertEquals(-1, chopper.chop(4, new int[]{1, 3, 5, 7}));
+        assertEquals(-1, chopper.chop(6, new int[]{1, 3, 5, 7}));
+        assertEquals(-1, chopper.chop(8, new int[]{1, 3, 5, 7}));
+    }
 }
